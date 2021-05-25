4e4efa2f30cdf88c2201ab09701a6a059d270694
RaquelCFlores <cfloresraquel@gmail.com>
2021-05-25 15:56:21 -0500

    Commit de prueba

---
+++ ejercicio.js
@@ -0,0 +1,6 @@
+function suma (a, b) {
+    console.log("Probando la consola");
+    return a + b;
+}
+
+console.log(suma(1,2));
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+arch            #Muestra la arquitectura de máquina
+date            #muestra la fecha del sistema
+dmidecode -q      #muestra el hardware del sistema
